700e7988b87be2678af2fd4cdc568e300c072b78
Rafael Pivatto <rafael.pivatto@gmail.com>
2018-11-17 21:40:30 -0200

    improve starbase: some improvements and add a back button

--- applications/starbase/src/components/Character.vue
+++ applications/starbase/src/components/Character.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col-md-4" @click="switchCharacter">
+    <div class="col-md-4">
         <div class="character-card">
             <div class="card-block">
                 <h4 class="card-title">{{character.name}}</h4>
@@ -7,32 +7,69 @@
                 <p class="card-text">Mass: {{character.mass}}kg</p>
                 <p class="card-text">Hair Color: {{character.hair_color}}</p>
                 <p class="card-text">Eye Color: {{character.eye_color}}</p>
+                <input type="hidden" id="lastId" v-model="actualId">
+                <button 
+                    class="btn btn-primary"
+                    @click="switchCharacter">
+                    Switch
+                </button>
+                <button 
+                    class="hide btn btn-success" 
+                    id="btnRollback"
+                    @click="rollbackCharacter();hideButton();">
+                    Rollback
+                </button>
             </div>
         </div>
     </div>
 </template>
 
+<style type="text/css">
+    @import url("./Character.css");
+</style>
+
 <script>
     export default {
-        props: ['id'], 
+        props: [
+            'id'
+        ], 
         data() {
             return {
                 character: {
+                },
+                actualId: {
+
+                },
+                lastId: {
 
                 }
             }
         },
         methods: {
-            fetchCharacter(id) {
+            fetchCharacter(id, event) {
                 fetch(`http://swapi.co/api/people/${id}`, {
                     method: 'GET'
                 })
                 .then(response => response.json())
-                .then(json => this.character = json)
+                .then(json => {
+                    this.character = json
+                    this.actualId = id;
+                    if (event) {
+                        const btnRollback = event.srcElement.parentElement.querySelector('#btnRollback')
+                        btnRollback.classList.remove('hide')
+                    }
+                })
             },
-            switchCharacter() {
+            switchCharacter(event) {
+                this.lastId = event.srcElement.parentElement.querySelector('#lastId').value
                 let random_id = Math.floor(Math.random() * 83) + 1
-                this.fetchCharacter(random_id)
+                this.fetchCharacter(random_id, event)
+            },
+            rollbackCharacter() {
+                this.fetchCharacter(this.lastId)
+            },
+            hideButton() {
+                event.srcElement.classList.add('hide')
             }
         },
         created() {
