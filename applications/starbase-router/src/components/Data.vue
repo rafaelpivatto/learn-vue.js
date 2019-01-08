c5ba0027074a3bd114bcbcc83412e6c166eb77fe
Rafael Pivatto <rafael.pivatto@gmail.com>
2019-01-08 21:49:20 -0200

    Adding route request to api

--- applications/starbase-router/src/components/Data.vue
+++ applications/starbase-router/src/components/Data.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="">
-        Type: {{type}}
+        {{items}}
     </div>
 </template>
 
@@ -8,8 +8,32 @@
 export default {
  data() {
      return {
-         type: this.$route.params.type
+         type: this.$route.params.type,
+         items: []
      }
- }   
+ },
+ watch: {
+    '$route': 'fetchItems'
+ },
+ methods: {
+     fetchItems() {
+         this.items = []
+         this.type = this.$route.params.type
+         let initials_ids = [1, 13, 14]
+
+         for (let i in initials_ids) {
+             let id = initials_ids[i]
+             console.log('id', id);
+             fetch(`http://swapi.co/api/${this.type}/${id}`,{
+                method: 'GET' 
+             })
+             .then(response => response.json())
+             .then(json => this.items.push(json))
+         }
+     }
+ },
+ created() {
+     this.fetchItems()
+ },
 }
 </script>
