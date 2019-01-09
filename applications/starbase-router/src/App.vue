d35ed12233abba71314507b3183295ed8a2b7682
Rafael Pivatto <rafael.pivatto@gmail.com>
2019-01-08 22:25:04 -0200

    add a challenge Idea

--- applications/starbase-router/src/App.vue
+++ applications/starbase-router/src/App.vue
@@ -3,7 +3,8 @@
         <h3>Starbase Router</h3>
         <h4>
             <router-link class="link" to="/data/people">Meet the Characters</router-link> | 
-            <router-link class="link" to="/data/planets">Explore the Planets</router-link>
+            <router-link class="link" to="/data/planets">Explore the Planets</router-link> | 
+            <router-link class="link" to="/data/vehicles">Explore the Vehicles</router-link>
         </h4>
         <router-view></router-view>
     </div>
