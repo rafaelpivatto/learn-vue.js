52d69eb578b4dbda3d3e5cd256c3b754ae7623f9
Rafael Pivatto <rafael.pivatto@gmail.com>
2019-01-09 23:01:28 -0200

    Adding methods to add and remove joke

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -2,13 +2,15 @@
     <div id="app">
         <h3>Got jokes?</h3>
         <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+        <button class="btn btn-primary" @click="addJoke">Add a Joke</button>
         <br>
         <br>
         <div class="col-md-12">
             <Joke 
                 v-for="(joke, index) in $store.state.jokes"
                 :joke="joke"
-                key="index"
+                :index="index"
+                :key="index"
             />
         </div>        
     </div>
@@ -20,7 +22,8 @@ import Joke from './Joke.vue'
 
 export default {
     methods: mapActions ([
-        'initJokes'
+        'initJokes',
+        'addJoke'
     ]),
     components: {
         Joke
--- applications/jokester/src/store/actions.js
+++ applications/jokester/src/store/actions.js
@@ -9,3 +9,15 @@ export const initJokes = ({commit}) => {
     .then(response => response.json())
     .then(json => commit(types.INIT_JOKES, json))
 }
+
+export const addJoke = ({commit}) => {
+    fetch('https://official-joke-api.appspot.com/random_joke', {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => commit(types.ADD_JOKE, json))
+}
+
+export const removeJoke = ({commit}, index) => {
+    commit(types.REMOVE_JOKE, index)
+}
