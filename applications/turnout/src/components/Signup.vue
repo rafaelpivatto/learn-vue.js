d00cb9ec025c153521fedb422793c3f8c6de16dc
Rafael Pivatto <rafael.pivatto@gmail.com>
2019-01-14 22:31:48 -0200

    Adding a sing-up

--- applications/turnout/src/components/Signup.vue
+++ applications/turnout/src/components/Signup.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="form-inline">
-        <h3>Sign In</h3>
+        <h3>Sign Up</h3>
         <div class="form-group">
     	    <input 
                 type="text" 
@@ -15,9 +15,9 @@
                 v-model="password"
             />
             <br>
-            <button class="btn btn-primary" @click="signIn">Sign In</button>
+            <button class="btn btn-primary" @click="signUp">Sign Up</button>
         </div>
-        <router-link to="/signup">Not a user? Sign up</router-link>
+        <router-link to="/signin">Already a user? Sign in</router-link>
         <br>
         <p>{{error.message}}</p>
     </div>
@@ -37,9 +37,9 @@ export default {
         }
     },
     methods: {
-        signIn() {
+        signUp() {
             firebaseApp.auth()
-            .signInWithEmailAndPassword(this.email, this.password)
+            .createUserWithEmailAndPassword(this.email, this.password)
             .catch(error => {
                 this.error = error
             })
