15f01755b1287666d28101fa95fe2822a4c71183
Rafael Pivatto <rafael.pivatto@gmail.com>
2019-01-15 23:24:24 -0200

    Adding event item and finish the application

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -10,15 +10,23 @@
         <hr>
         <AddEvent />
         <hr>
-        {{$store.state}}
+        <div class="col-md-12">
+            <EventItem
+                v-for="(event_item, index) in this.$store.state.events"
+                :event="event_item"
+                :key="index"
+            />
+        </div>
+        
 
     </div>
 </template>
 
 <script>
 
-import { firebaseApp } from '../firebaseApp'
+import { firebaseApp, eventsRef } from '../firebaseApp'
 import AddEvent from './AddEvent.vue'
+import EventItem from './EventItem.vue'
 
 export default {
     methods: {
@@ -28,7 +36,17 @@ export default {
         }
     },
     components: {
-        AddEvent
+        AddEvent,
+        EventItem
+    },
+    mounted() {
+        eventsRef.on('value', snap => {
+            let events = []
+            snap.forEach(event => {
+                events.push(event.val())
+            })
+            this.$store.dispatch('setEvents', events)
+        })
     },
 }
 </script>
